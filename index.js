1e4a48d3072da56ead1bc9372c0e3bbbefcbb32a
Rocko <Rockokaine@gmail.com>
2021-08-19 15:57:33 -0700

    express added

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express'),
+      path = require('path'),
       app = express();
 
+      app.set('view engine', 'ejs');
+      app.set('views', path.join(__dirname,"/views"));
+      app.use(express.static(path.join(__dirname, 'incs')));
 
-    app.use(()=>{
-        console.log('we got a new request');
+
+
+    app.get('/', (req, res)=>{
+        res.render("partials/home.ejs");
     });
 
 
     app.listen(3000, ()=>{
-      console.log('Listening on port 3000');
+      console.log('Listening on port 3000!');
     });
